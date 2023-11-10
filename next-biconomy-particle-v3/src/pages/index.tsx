f2dd428b0f0da9c39a72c8f51a0dfa2e2a7fdf9d
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2023-11-10 14:21:58 +0530

    Working on Biconomy Ethers sdk example

--- next-biconomy-particle-v3/src/pages/index.tsx
+++ next-biconomy-particle-v3/src/pages/index.tsx
@@ -1,17 +1,123 @@
 import React, { useState, useEffect } from "react";
+import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import {
+  BiconomySmartAccountV2,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
+import { Wallet, providers, ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
+import {
+  ECDSAOwnershipValidationModule,
+  DEFAULT_ECDSA_OWNERSHIP_MODULE,
+} from "@biconomy/modules";
+import { contractABI } from "../contract/contractABI";
+
+// Create a provider for the Polygon Mumbai network
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc.ankr.com/polygon_mumbai"
+);
+
+//Particle auth will require api keys which you can get from the Particle Dashboard. (https://dashboard.particle.network/)
+const particle = new ParticleAuthModule.ParticleNetwork({
+  //@ts-ignore
+  projectId: process.env.NEXT_PUBLIC_PARTICLE_AUTH_PROJECT_ID,
+  //@ts-ignore
+  clientKey: process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY,
+  //@ts-ignore
+  appId: process.env.NEXT_PUBLIC_PARTICLE_AUTH_APP_ID,
+  wallet: {
+    displayWalletEntry: true,
+    defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+  },
+});
+
+// Specify the chain ID for Polygon Mumbai
+let chainId = 80001; // Polygon Mumbai or change as per your preferred chain
+
+// Create a Bundler instance
+const bundler: IBundler = new Bundler({
+  // get from biconomy dashboard https://dashboard.biconomy.io/
+  // for mainnet bundler url contact us on Telegram
+  bundlerUrl: `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
+  chainId: ChainId.POLYGON_MUMBAI, // or any supported chain of your choice
+  entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+});
+
+// Create a Paymaster instance
+const paymaster: IPaymaster = new BiconomyPaymaster({
+  // get from biconomy dashboard https://dashboard.biconomy.io/
+  // Use this paymaster url for testing, you'll need to create your own paymaster for gasless transactions on your smart contracts.
+  paymasterUrl:
+    "https://paymaster.biconomy.io/api/v1/80001/-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
+});
 
 export default function Home() {
+  const [smartAccount, setSmartAccount] =
+    useState<BiconomySmartAccountV2 | null>(null);
+  const [smartAccountAddress, setSmartAccountAddress] = useState<string | null>(
+    null
+  );
+  const [count, setCount] = useState<string | null>(null);
+
+  const connect = async () => {
+    try {
+      console.log(process.env.NEXT_PUBLIC_PARTICLE_AUTH_PROJECT_ID)
+      const userInfo = await particle.auth.login();
+      console.log("Logged in user:", userInfo);
+      const particleProvider = new ParticleProvider(particle.auth);
+      const web3Provider = new ethers.providers.Web3Provider(
+        particleProvider,
+        "any"
+      );
+
+      const ecdsaModule = await ECDSAOwnershipValidationModule.create({
+        signer: web3Provider.getSigner(),
+        moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE,
+      });
+
+      let biconomySmartAccount = await BiconomySmartAccountV2.create({
+        chainId: ChainId.POLYGON_MUMBAI,
+        bundler: bundler,
+        paymaster: paymaster,
+        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+        defaultValidationModule: ecdsaModule,
+        activeValidationModule: ecdsaModule,
+      });
+
+      console.log(biconomySmartAccount);
+      setSmartAccount(biconomySmartAccount);
+      const address = await biconomySmartAccount.getAccountAddress();
+      console.log(address);
+      setSmartAccountAddress(address);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getCountId = async () => {
+    const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
+    const countId = await contractInstance.getCount();
+    setCount(countId.toString());
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
       <div className="text-[4rem] font-bold text-orange-400">
         Biconomy-Magic
       </div>
-      {/* {!smartAccount && (
+      {!smartAccount && (
         <button
           className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
           onClick={connect}
         >
-          Magic Sign in
+          Particle Sign in
         </button>
       )}
 
@@ -33,14 +139,14 @@ export default function Home() {
             <div className="flex flex-col justify-center items-center gap-4">
               <button
                 className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
-                onClick={incrementCount}
+                // onClick={incrementCount}
               >
                 Increment Count
               </button>
             </div>
           </div>
         </>
-      )} */}
+      )}
     </main>
   );
 }
